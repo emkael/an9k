9f986a186c727ae47443ddbf5c0a3604964802f6
emkael <emkael@tlen.pl>
2021-01-29 23:51:39 +0100

    Version number bump

--- Analizator9000/Analizator9000/Properties/AssemblyInfo.cs
+++ Analizator9000/Analizator9000/Properties/AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System.Resources;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Analizator9000")]
-[assembly: AssemblyCopyright("Copyright © Michał Klichowicz 2012-2015")]
+[assembly: AssemblyCopyright("Copyright © Michał Klichowicz 2012-2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -33,6 +33,6 @@ using System.Resources;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.11.0.0")]
-[assembly: AssemblyFileVersion("0.11.0.0")]
+[assembly: AssemblyVersion("0.12.0.0")]
+[assembly: AssemblyFileVersion("0.12.0.0")]
 [assembly: NeutralResourcesLanguageAttribute("")]
